727cd0eb4f2425d1a7eb50161d828c06eff502b3
hrldspnz <haroldespinoza823@gmail.com>
2021-09-12 18:52:49 -0600

    api

--- Administracion.BackEnd/Controllers/MovimientoController.cs
+++ Administracion.BackEnd/Controllers/MovimientoController.cs
@@ -11,17 +11,56 @@ namespace Administracion.BackEnd.Controllers
     [ApiController]
     public class MovimientoController : ControllerBase
     {
-        int[] t1 = new int[] { 1021, 450000, };
-        int[] t2 = new int[] { 1022, 32000 };
-        [HttpGet("ObtenerMov/{id}")]
-        public int[] Get(int id)
+        /*int[] t1 = new int[] { 1021, 450000, };
+        int[] t2 = new int[] { 1022, 32000 };*/
+        [HttpGet("ObtenerMov")]
+        public List<Model.Mov> Get()
         {
-            return id switch
-            {
-                1 => t1,
-                2 => t2,
-                _ => throw new NotSupportedException("El id no es valido")
-            };
+
+            var movs = GetMovs();
+            return movs;
+            
         }
+        [HttpGet("ObtenerMov/{id}")]
+        public Model.Mov Getm(int id)
+        {
+
+            var movs = GetMovs();
+            foreach (Model.Mov mov in movs){
+                if (id == mov.Numtran) 
+                {
+                    return mov;
+                }
+            };
+            return null;
+            
+        }
+
+
+        public static List<Model.Mov> GetMovs() 
+        {
+            List<Model.Mov> movimientos = new List<Model.Mov> {
+                new Model.Mov
+                {
+                    Numtran = 1,
+                    Monto = 2,
+                    Tipo = "Deposito",
+                    Descripcion = "cuentas",
+                    fecha = "10may2021"
+
+                },
+                new Model.Mov
+                {
+                    Numtran = 2,
+                    Monto = 3,
+                    Tipo = "Retiro",
+                    Descripcion = "cuentas",
+                    fecha = "11may2021"
+
+                },
+                };
+            return movimientos;
+        }
+
     }
 }
---
+++ Administracion.BackEnd/Model/Mov.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Administracion.BackEnd.Model
+{
+    public class Mov
+    {
+        public int Numtran { get; set; }
+        public int Monto { get; set; }
+        public string Tipo { get; set; }
+        public string Descripcion { get; set; }
+        public string fecha { get; set; }
+    }
+}
