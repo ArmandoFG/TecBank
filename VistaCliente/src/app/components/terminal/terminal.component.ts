6a5878249fe4da94496fcc145d3cf6c9394fb690
FALCH2000 <faleiva.ch@gmai.com>
2021-09-16 00:32:38 -0600

    Documentacion pagina principal

--- VistaCliente/src/app/components/terminal/terminal.component.ts
+++ VistaCliente/src/app/components/terminal/terminal.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Cuenta } from 'src/app/models/cuenta';
 import { CuentasService } from 'src/app/services/cuentas.service';
-
+/**
+  * @fileoverview Carga la pagina principal, esta se carga
+  * despues de hacer un login
+  * @author Fatima Leiva
+  */
 @Component({
   selector: 'terminal',
   templateUrl: './terminal.component.html',
@@ -9,11 +13,14 @@ import { CuentasService } from 'src/app/services/cuentas.service';
   providers: [CuentasService]
 })
 export class TerminalComponent implements OnInit {
-
+  /** @constructor */
   constructor(
-    private _CuentasService: CuentasService
+    private _CuentasService: CuentasService 
   ) { }
-
+  
+    /**Estaria obteniendo las cuentas que hay asociadas
+     * a un mismo usuario
+     */
   ngOnInit(): void {
     this._CuentasService.getCuentas;
   }
