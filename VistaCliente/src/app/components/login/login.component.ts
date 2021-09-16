17160b9c3b98323649a6c4e36a384e1f5ce86752
FALCH2000 <faleiva.ch@gmai.com>
2021-09-16 00:18:12 -0600

    Comentarios login

--- VistaCliente/src/app/components/login/login.component.ts
+++ VistaCliente/src/app/components/login/login.component.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
-
+ /**
+  * @fileoverview Login del usuario, obtiene el usuario y su contraseña
+  * @author Fatima Leiva
+  */
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit, DoCheck, OnDestroy {
-
+/** @constructor */
   constructor() { }
-
+  /**
+   * Se ejecuta cuando se ejecuta el proyecto, una vez despues
+   * del constructor
+   */
   ngOnInit(): void {
     console.log("se inicializa");
   }
-
+  /**
+   * Se ejecuta cuando se cambia algo en el programa
+   */
   ngDoCheck(): void{
     console.log("docheck ejecutado");
   }
@@ -20,11 +29,16 @@ export class LoginComponent implements OnInit, DoCheck, OnDestroy {
   ngOnDestroy(): void{
     
   }
-
+  /**
+   * Valida que el usuario existe efectivamente
+   */
   validarUsuario(){
     console.log("Se está validando usuario, existe o no existe");
   }
-
+  /**
+   * Se ejecuta cuando el botón de inicio es presionado
+   * y abre la ventana del cliente
+   */
   abrirPaginaPrincipal(){
     console.log("Abriendo principal");
   }
