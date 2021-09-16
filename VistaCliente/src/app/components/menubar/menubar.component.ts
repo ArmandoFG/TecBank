8adc2ce17a55e43765fa32d742d4522c5d973cb8
FALCH2000 <faleiva.ch@gmai.com>
2021-09-16 00:20:33 -0600

    Documentación menubar

--- VistaCliente/src/app/components/menubar/menubar.component.ts
+++ VistaCliente/src/app/components/menubar/menubar.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+/**
+  * @fileoverview Contiene el menú de navegacion
+  * se implementa en las paginas dentro de la vista excepto
+  * en el login
+  * @author Fatima Leiva
+  */
 @Component({
   selector: 'menubar',
   templateUrl: './menubar.component.html',
   styleUrls: ['./menubar.component.css']
 })
 export class MenubarComponent implements OnInit {
-
+  /**@constructor */
   constructor() { }
 
   ngOnInit(): void {
