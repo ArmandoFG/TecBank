3b392fe2db5e573d4bd933a7d769b996eb0a7896
FALCH2000 <faleiva.ch@gmai.com>
2021-09-15 23:32:33 -0600

    Actualización de permisos CORS

--- Administracion.BackEnd/Startup.cs
+++ Administracion.BackEnd/Startup.cs
@@ -19,12 +19,19 @@ namespace Administracion.BackEnd
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options => {
+                options.WithOrigins("http://localhost:4200");
+                options.AllowAnyMethod();
+                options.AllowAnyHeader();
+
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
